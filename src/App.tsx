fadbda81b2b48454768fad2ee92e95ca60d1f1a2
Denys <zorosmg5@mail.com>
2025-03-05 13:56:30 +0200

    add solutions

--- src/App.tsx
+++ src/App.tsx
@@ -100,6 +100,7 @@ export const App: React.FC = () => {
 
   return (
     <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
       <header className="todoapp__header">
         <button
           type="button"
